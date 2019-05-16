c918cf8d6a0bcef4edde3fa7b800ef33add2d439
Liozart <bobponay@gmail.com>
2019-05-16 10:46:26 +0200

    added logo prefab, removed buggy animations previews

--- js/classes.js
+++ js/classes.js
@@ -25,13 +25,6 @@ class Banner {
     createBanner(){
         var self = this;
         return new Promise(function(resolve, reject){
-            //Create back plane
-            var geometry = new THREE.PlaneGeometry(700, 130);
-            var material = new THREE.MeshLambertMaterial( { color: self.color_plane, side: THREE.DoubleSide,
-            transparent: true, opacity: 1.0} );
-            self.mesh_plane = new THREE.Mesh( geometry, material );
-            self.mesh_plane.castShadow = true;
-            self.mesh_plane.receiveShadow = true;
             //Create text
             var materialFront = new THREE.MeshBasicMaterial( { color: self.color_text_in } );
             var materialSide = new THREE.MeshBasicMaterial( { color: self.color_text_out } );
@@ -53,6 +46,14 @@ class Banner {
                 self.mesh_text.castShadow = true;
                 self.mesh_text.receiveShadow = true;
                 var box = new THREE.Box3().setFromObject( self.mesh_text ).getSize();
+
+                //Create back plane
+                var geometry = new THREE.PlaneGeometry((box.x + 40), 130);
+                var material = new THREE.MeshLambertMaterial( { color: self.color_plane, side: THREE.DoubleSide,
+                    transparent: true, opacity: 1.0} );
+                self.mesh_plane = new THREE.Mesh( geometry, material );
+                self.mesh_plane.castShadow = true;
+                self.mesh_plane.receiveShadow = true;
 
                 //Merge the two objects
                 self.mesh_group.add( self.mesh_plane );
@@ -129,7 +130,7 @@ class Banner {
  */
 
 class Ellipse {
-    constructor(text) {
+    constructor(path) {
         this.color_cylinder = new THREE.Color();
         this.color_cylinder.setHex("0x00ff00");
 
@@ -139,11 +140,9 @@ class Ellipse {
         this.color_text_out = new THREE.Color();
         this.color_text_out.setHex("0x0000ff");
 
-        this.ellipse_text = text;
+        this.ellipse_texture = '3dObjects/temp/' + path;
 
         this.mesh_ellipse = null;
-        this.mesh_text = null;
-        this.mesh_group = new THREE.Group();
     }
 
     /*
@@ -151,56 +150,20 @@ class Ellipse {
     */
     createEllipse(){
         var self = this;
-        return new Promise(function(resolve, reject){
-            var curve = new THREE.EllipseCurve(
-                0,  0,            // ax, aY
-                200, 100,           // xRadius, yRadius
-                0,  2 * Math.PI,  // aStartAngle, aEndAngle
-                false,            // aClockwise
-                0                 // aRotation
-            );
+        return new Promise(function(resolve, reject) {
+            var geometry = new THREE.CylinderGeometry(150, 150, 15, 100);
+            var texture = new THREE.TextureLoader().load( self.ellipse_texture );
 
-            var points = curve.getPoints( 50 );
-            var geometry = new THREE.BufferGeometry().setFromPoints( points );
+            var material = new THREE.MeshBasicMaterial({
+                map: texture
+            });
+            material.transparent = true;
 
-            var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
-            // Create the final object to add to the scene
-            self.mesh_ellipse = new THREE.Line( geometry, material );
+            self.mesh_ellipse = new THREE.Mesh(geometry, material);
             self.mesh_ellipse.castShadow = true;
             self.mesh_ellipse.receiveShadow = true;
+            //Add object to arrays and scene
             resolve(self.mesh_ellipse);
-            //Create text
-            var materialFront = new THREE.MeshBasicMaterial( { color: self.color_text_in } );
-            var materialSide = new THREE.MeshBasicMaterial( { color: self.color_text_out } );
-            var materialArray = [ materialFront, materialSide ];
-            var loader = new THREE.FontLoader();
-            loader.load( "fonts/helvetiker_regular.typeface.json", function ( font ) {
-                var geometry = new THREE.TextGeometry( self.ellipse_text, {
-                    font: font,
-                    size: 80,
-                    height: 5,
-                    curveSegments: 12,
-                    bevelEnabled: true,
-                    bevelThickness: 8,
-                    bevelSize: 6,
-                    bevelOffset: 0,
-                    bevelSegments: 5
-                });
-                self.mesh_text = new THREE.Mesh(geometry, materialArray);
-                self.mesh_text.castShadow = true;
-                self.mesh_text.receiveShadow = true;
-                var box = new THREE.Box3().setFromObject( self.mesh_text ).getSize();
-
-                //Merge the two objects
-                self.mesh_group.add( self.mesh_cylinder );
-                self.mesh_group.add( self.mesh_text );
-                //Center the text
-                self.mesh_cylinder.position.set((box.x / 2), (box.y / 3), -20.0);
-                self.mesh_group.position.set(-(box.x / 2), 100, 0);
-                //return the group
-                resolve(self.mesh_group);
-            });
         });
     }
 
